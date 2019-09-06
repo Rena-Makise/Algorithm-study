39a8140da5276902d141aee56872222492d5cdc0
Rena <ckdwomn@live.com>
2019-09-06 16:03:44 +0900

    EditList

--- BinarySearch/Budget/Solution.java
+++ BinarySearch/Budget/Solution.java
@@ -2,11 +2,55 @@ import java.util.*;
 
 class Solution {
     public static void main(String[] args) {
-        
+        System.out.println(solution(new int[] { 120, 110, 140, 150 }, 485));
     }
-    
+
     public static int solution(int[] budgets, int M) {
         int answer = 0;
+        Arrays.sort(budgets);
+
+        int length = budgets.length;
+        int min = 0;
+        int max = M;
+        int mid = 0;
+        int premid = 0;
+        long sum = 0;
+
+        for (int i : budgets) {
+            sum += i;
+        }
+
+        if (sum < M) {
+            answer += budgets[length - 1];
+            return answer;
+        } else {
+            while (true) {
+                sum = 0;
+                mid = (max + min) / 2;
+
+                if (mid == premid) {
+                    break;
+                }
+
+                for (int i = 0; i < length; ++i) {
+                    if (mid <= budgets[i]) {
+                        sum += mid * (length - i);
+                        break;
+                    } else {
+                        sum += budgets[i];
+                    }
+                }
+
+                if (sum <= M) {
+                    min = mid;
+                } else {
+                    max = mid;
+                }
+                premid = mid;
+            }
+            answer = mid;
+        }
+
         return answer;
     }
 }
---
+++ Graph/MostFar/Solution.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+/**
+ * 하나의 정점에서 다른 모든 정점까지의 최단 경로를 구하는 문제의 경우
+ * 다익스트라 알고리즘을 활용할 수 있다.
+ * https://hsp1116.tistory.com/42 참고
+ */
+class Solution {
+    static List<Integer>[] list;
+    static int[] dist;
+    static int max;
+
+    public static void main(String[] args) {
+        System.out.println(solution(6, new int[][] { { 3, 6 }, { 4, 3 }, { 3, 2 }, { 1, 3 }, { 1, 2 }, { 2, 4 }, { 5, 2 } }));
+    }
+
+    public static void path (int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        dist[0] = 0;
+        while (!queue.isEmpty()) {
+            int vertex = queue.remove();
+            for (Integer i : list[vertex]) {
+                if (dist[i] > dist[vertex] + 1 && dist[i] == 9999) {
+                    dist[i] = dist[vertex] + 1;
+                    queue.add(i);
+                    max = Math.max(max, dist[i]);
+                }
+            }
+        }
+    }
+
+    public static int solution(int n, int[][] edge) {
+        int answer = 0;
+        list = new ArrayList[n];
+
+        for (int i = 0; i < n; ++i) list[i] = new ArrayList<>();
+        for (int i = 0; i < edge.length; ++i) {
+            list[edge[i][0] - 1].add(edge[i][1] - 1);
+            list[edge[i][1] - 1].add(edge[i][0] - 1);
+        }
+        dist = new int[n];
+        Arrays.fill(dist, 9999);
+        path(n);
+        for (int i = 0; i < dist.length; i++) {
+            if (max == dist[i]) answer++;
+        }
+        return answer;
+    }
+}
